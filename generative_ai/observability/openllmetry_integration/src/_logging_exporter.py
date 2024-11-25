34103563a5c5db721a616dc17e2cd6cb6f9d2a7a
Michael Aaron Safyan <michaelsafyan@google.com>
2024-11-25 12:47:03 -0500

    Checkpoint current progress.

--- generative_ai/observability/openllmetry_integration/src/_logging_exporter.py
+++ generative_ai/observability/openllmetry_integration/src/_logging_exporter.py
@@ -33,9 +33,14 @@ repo (https://github.com/GoogleCloudPlatform/opentelemetry-operations-python) to
 determine if there is a Cloud Logging exporter; if so, prefer to use that.
 """
 
+import hashlib
+import os
 from typing import Sequence
 
 from google.cloud.logging_v2 import LogEntry
+from google.logging.type import log_severity_pb2
+from google.protobuf import timestamp_pb2
+from opentelemetry.api._logs.severity import SeverityNumber
 from opentelemetry.sdk._logs.export import LogExporter
 from opentelemetry.sdk._logs.export import LogExportResult
 from opentelemetry.sdk._logs import LogData
@@ -76,27 +81,109 @@ class _OtlpLog(object):
 
 def _compute_log_name(olog: _OtlpLog):
     """Helper for '_to_gcp_format' below."""
-    return None
+    return 'projects/{}/logs/otlpgenai'.format(olog.project)
 
 
 def _compute_labels(olog: _OtlpLog):
     """Helper for '_to_gcp_format' below."""
-    return None
+    return _LABELS
+
+
+def _add_to_hasher(hasher, properties):
+    for property_name, property_value in properties:
+        if property_value is None:
+            property_value = '(null)'
+        hasher.update('{}={}'.format(property_name, property_value))
 
 
 def _compute_insert_id(olog: _OtlpLog):
     """Helper for '_to_gcp_format' below."""
+    hash_algo = os.getenv('INSERT_ID_HASH_ALGORITHM', 'sha1')
+    hasher = hashlib.new(hash_algo, usedforsecurity=False)
+    attributes = olog.log.attributes or {}
+    event_name = attributes.get('event.name', '')
+    _add_to_hasher(
+        hasher,
+        [
+          ('event_name', event_name),
+          ('timestamp', olog.log.timestamp),
+          ('trace_id', olog.log.trace_id),
+          ('span_id', olog.log.span_id)
+        ])
+
+    for key in sorted(attributes.keys()):
+        if key != 'event.name':
+            entry = 'attributes["{}"]={}'.format(key, attributes[key])
+            hasher.update(entry)
+    
+    resource_attributes = olog.resource.attributes
+    for key in sorted(resource_attributes.key):
+        value = resource_attributes[key]
+        entry = 'resource.attributes["{}"]={}'.format(key, value)
+        hasher.update(entry)
+    
+    return hasher.hexdigest()
+
+
+def _otlp_severity_to_gcp_severity(oseverity_number):
+    if (oseverity_number == SeverityNumber.UNSPECIFIED):
+        return log_severity_pb2.LogSeverity.DEFAULT
+
+    if ((oseverity_number >= SeverityNumber.TRACE) and
+        (oseverity_number < SeverityNumber.INFO)):
+        return log_severity_pb2.LogSeverity.DEBUG
+
+    if ((oseverity_number >= SeverityNumber.DEBUG) and
+        (oseverity_number < SeverityNumber.INFO)):
+        return log_severity_pb2.LogSeverity.DEBUG
+
+    if ((oseverity_number >= SeverityNumber.INFO) and
+        (oseverity_number < SeverityNumber.WARN)):
+        return log_severity_pb2.LogSeverity.INFO
+
+    if ((oseverity_number >= SeverityNumber.WARN) and
+        (oseverity_number < SeverityNumber.ERROR)):
+        return log_severity_pb2.LogSeverity.WARNING
+
+    if ((oseverity_number >= SeverityNumber.ERROR) and
+        (oseverity_number < SeverityNumber.FATAL)):
+        return log_severity_pb2.LogSeverity.ERROR
+
+    if (oseverity_number >= SeverityNumber.FATAL):
+        return log_severity_pb2.LogSeverity.CRITICAL
+  
     return None
+
+
+def _otlp_severity_text_to_gcp_severity(oseverity_text):
+    prefix_map = {
+        'trace': log_severity_pb2.LogSeverity.DEBUG,
+        'debug': log_severity_pb2.LogSeverity.DEBUG,
+        'info': log_severity_pb2.LogSeverity.INFO,
+        'warn': log_severity_pb2.LogSeverity.WARNING,
+        'error': log_severity_pb2.LogSeverity.ERROR,
+        'fatal': log_severity_pb2.LogSeverity.CRITICAL,
+    }
+    lower_text = oseverity_text.lower()
+    while lower_text[-1].isdigit():
+        lower_text = lower_text[0:len(lower_text)-1]
+    return prefix_map.get(lower_text, log_severity_pb2.LogSeverity.DEFAULT)
 
 
 def _compute_severity(olog: _OtlpLog):
     """Helper for '_to_gcp_format' below."""
+    oseverity_number = olog.severity_number
+    if oseverity_number is not None:
+        return _otlp_severity_to_gcp_severity(oseverity_number)
+    oseverity_text = olog.severity_text
+    if oseverity_text:
+        return _otlp_severity_text_to_gcp_severity(oseverity_text)
     return None
 
 
 def _compute_timestamp(olog: _OtlpLog):
     """Helper for '_to_gcp_format' below."""
-    return None
+    return timestamp_pb2.Timestamp(nanos=olog.timestamp)
 
 
 def _compute_resource(olog: _OtlpLog):
